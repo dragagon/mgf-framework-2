fb69b1b5ba73211d8b5229d14bd91884f2db5442
Christian Richards <cjrgaming@gmail.com>
2018-01-04 16:08:03 -0700

    Video 22 - Entity Framework, Context, and MSSQL/MySQL Config

---
+++ MultiplayerGameFramework/MGF.DataEntities/DataEntities/Stat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGF.DataEntities
+{
+    public class Stat
+    {
+        public int StatId { get; set; }
+        public String Name { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ MultiplayerGameFramework/MGF.DataEntities/MGFContext.cs
@@ -0,0 +1,32 @@
+﻿using MGF.DataEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGF
+{
+#if MYSQL
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class MGFContext : DbContext
+    {
+
+        public MGFContext() : base("name=MySqlDbConnectionString")
+        {
+        }
+#else
+    public class MGFContext : DbContext
+    {
+
+        public MGFContext() : base("name=MsSqlDbConnectionString")
+        {
+        }
+#endif
+
+        // Define Entities Here
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Stat> Stats { get; set; }
+    }
+}
